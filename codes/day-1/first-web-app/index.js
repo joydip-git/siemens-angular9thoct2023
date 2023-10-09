7354b05e737f431a17ec3bcc53f0f9a5457f8ebc
Joydip <joydip.mondal@gmail.com>
2023-10-09 14:05:05 +0530

    array and functions in JS

--- codes/day-1/first-web-app/index.js
+++ codes/day-1/first-web-app/index.js
@@ -79,3 +79,56 @@ try {
 
 //if(a!=b) => the value of a and b are different or not
 //if(a !== b) => the data type and value of a and b are different or not
+
+//functions in JS
+
+console.log(sayHi('anil')) //<-- functions written using declaration can be called before the function
+//function declaration
+function sayHi(name) {
+    return "Hi " + name
+}
+console.log(sayHi('sunil'))
+
+//sayBye('joydip') <-- functions written using expression can't be called before the function
+//function expression (inline function)
+var sayBye = function (name) {
+    console.log("Bye " + name)
+}
+sayBye('joydip')
+
+//array in JS
+var numbers = [10, 20, 30, 40]
+//var numbers = new Array(10, 20, 30, 40)
+
+numbers.push(50, 60)
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+console.log('\nafter deletion\n')
+numbers.splice(1, 2)
+for (var val of numbers) {
+    console.log(val)
+}
+
+console.log('\nafter replacement\n')
+numbers.splice(2, 2, 500, 600)
+for (var index in numbers) {
+    console.log(numbers[index])
+}
+
+console.log('\ncopy of values\n')
+var copy = numbers.slice(0, 2)
+for (const num of copy) {
+    console.log(num)
+}
+
+console.log('\nafter adding at the beginning\n')
+numbers.unshift(100, 200)
+for (const index in numbers) {
+    console.log(numbers[index])
+}
+
+//filter, sort, map, find, findIndex, reduce etc.
+
+
--- codes/day-1/first-web-app/main.ts
+++ codes/day-1/first-web-app/main.ts
@@ -2,4 +2,4 @@ const value = 100
 //value = 200
 
 var myData: number = 200
-myData = 'joydip'
+//myData = 'joydip'
